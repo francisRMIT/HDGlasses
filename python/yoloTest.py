914f0712ac641af3316d549a3bae1d29c86d632c
AydenRMIT <s4003937@student.rmit.edu.au>
2026-04-20 13:43:25 +1000

    what kind of awesome is this

--- python/yoloTest.py
+++ python/yoloTest.py
@@ -1,21 +1,36 @@
-#import cv2
-#import time
-#import keyboard
+import cv2
+import time
+import keyboard
 from ultralytics import YOLO
 
-#capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(0)
 model = YOLO("yolo26n.pt")
-#i = 0
-results = model.predict(source=0, show=True)
 
-#def YOLOingIt():
-        #ret, frame = capture.read()
-        #results = model.predict(frame)
-        #for r in results:
-        #        boxes = r.boxes
-        #for box in boxes:
-        #        box
+
+def YOLOingIt():
+        ret, frame = capture.read()
+        
+        results = model.predict(frame, verbose=False)
+        for r in results:
+                cv2.imshow("camera", r.plot())
+                cv2.waitKey(1)
+                boxes = r.boxes
+        for box in boxes:
+                clsid = box.cls.int()
+                name = r.names[clsid.item()]
+                print(name)
 
  
-baller = 100
 
+while True:
+        if keyboard.is_pressed("space"):
+                cv2.destroyAllWindows()
+                YOLOingIt()
+                time.sleep(0.5)
+
+        if keyboard.is_pressed("q"):
+                cv2.destroyAllWindows()
+                time.sleep(0.5)
+
+        if keyboard.is_pressed("esc"):
+                break
