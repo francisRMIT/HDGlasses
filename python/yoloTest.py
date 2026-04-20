83ec095b004d9d6f389d283441d3b5eb2511347a
AydenRMIT <s4003937@student.rmit.edu.au>
2026-04-20 13:52:40 +1000

    even more awesome

--- python/yoloTest.py
+++ python/yoloTest.py
@@ -2,14 +2,13 @@ import cv2
 import time
 import keyboard
 from ultralytics import YOLO
+from playsound3 import playsound
 
 capture = cv2.VideoCapture(0)
 model = YOLO("yolo26n.pt")
 
-
 def YOLOingIt():
         ret, frame = capture.read()
-        
         results = model.predict(frame, verbose=False)
         for r in results:
                 cv2.imshow("camera", r.plot())
@@ -17,8 +16,9 @@ def YOLOingIt():
                 boxes = r.boxes
         for box in boxes:
                 clsid = box.cls.int()
+                conf = round(box.conf.item(),2)
                 name = r.names[clsid.item()]
-                print(name)
+                print(name, " ", conf)
 
  
 
@@ -26,6 +26,7 @@ while True:
         if keyboard.is_pressed("space"):
                 cv2.destroyAllWindows()
                 YOLOingIt()
+                playsound("C:/RMIT/2026/Design 3/HDGlasses/[Pigsy]What kind of object is this.mp3")
                 time.sleep(0.5)
 
         if keyboard.is_pressed("q"):
